209aca67f3df9c0638b2936d261a4d68509a2d10
pyorunochron@gmail.com <}\\XHL4)6(:Pdh`^>
2018-10-24 14:19:31 -0400

    Okamoto done ;)

--- Okamoto.py
+++ Okamoto.py
@@ -9,38 +9,40 @@ class Okamoto():
         group.paramgen(bitsize)
 
     def keyGeneration(self):
-        g = group.randomGen()
-        a = group.random()
-        pk = {'g': g, 'A': g ** a}
-        sk = a
+        g1 = group.randomGen()
+        g2 = group.randomGen()
+        a1 = group.random()
+        a2 = group.random()
+        A = (g1 ** a1) * (g2 ** a2)
+        pk = {'g1': g1, 'g2': g2, 'A': A}
+        sk = {'a1': a1, 'a2': a2}
         return pk, sk
 
     def generateX(self, pk):
-        x = group.random()
-        X = pk['g'] ** x
-        return X, x
+        x1 = group.random()
+        x2 = group.random()
+        X = (pk['g1'] ** x1) * (pk['g2'] ** x2)
+        return X, x1, x2
 
     def challenge(self):
         c = group.random()
         return c
 
-    def response(self, x, c, sk):
-        s = (x + c * sk)
-        return s
+    def response(self, x1, x2, c, sk):
+        s1 = (x1 + c * sk['a1'])
+        s2 = (x2 + c * sk['a2'])
+        return s1, s2
 
-    def verify(self, s, X, pk, c):
-        return (pk['g'] ** s) == (X * pk['A'] ** c)
-
-
-
+    def verify(self, s1, s2, X, pk, c):
+        return ((pk['g1'] ** s1) * (pk['g2'] ** s2)) == (X * pk['A'] ** c)
 
 group1 = IntegerGroupQ()
 scheme = Okamoto(group1, 256)
 (pk, sk) = scheme.keyGeneration()
-(X, x) = scheme.generateX(pk)
+(X, x1, x2) = scheme.generateX(pk)
 c = scheme.challenge()
-s = scheme.response(x, c, sk)
-decision = scheme.verify(s, X, pk, c)
+(s1, s2) = scheme.response(x1, x2, c, sk)
+decision = scheme.verify(s1, s2, X, pk, c)
 if (decision):
     print("GOod")
 else:
