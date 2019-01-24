2cfa096937a5bf3e2ad4f3a00dfdf286c409267e
pyorunochron@gmail.com <}\\XHL4)6(:Pdh`^>
2019-01-24 15:11:58 -0500

    PReprare to die

--- 6_GohJareckiSign.py
+++ 6_GohJareckiSign.py
@@ -8,14 +8,14 @@ class Signer:
         self.a = sk['a']
 
     def sign(self, m):
+        A = self.g ** self.a
         r = group.random()
         h = group.hash(m, r)
+        x = groupQ.random()
         H = h ** self.a
-        x = group.random()
         X = h ** x
         R = self.g ** x
-        A = self.g ** self.a
-        c = group.hash(m, r, X, A, H, R)
+        c = groupQ.hash(m, r, X, A, H, R)
         s = x + self.a * c
         return {'R': r, 'X': X, 'H': H, 's': s, 'r': r}
 
@@ -28,22 +28,25 @@ class Verifier:
 
     def verify(self, sig, m):
         h = group.hash(m, sig['r'])
-        c = group.hash(m, sig['r'], sig['X'], self.A, sig['H'], sig['R'])
-        left = self.g ** sig['s'] == sig['R'] * sig['H'] ** c  # wyjatek tutaj leci :(
+        c = groupQ.hash(m, sig['r'], sig['X'], self.A, sig['H'], sig['R'])
+        left = (self.g ** sig['s']) == sig['R'] * (self.A ** c)  # wyjatek tutaj leci :(
         right = h ** sig['s'] == sig['X'] * sig['H'] ** c
         return left == right
 
 
 def keygen():
     g = group.randomGen()
-    a = group.random()
+    a = groupQ.random()
     sk = {'g': g, 'a': a}
     pk = {'g': g, 'A': g ** a}
     return pk, sk
 
 
-group = IntegerGroupQ(1024)
+group = IntegerGroupQ()
 group.paramgen(1024)
+groupQ = IntegerGroupQ()
+groupQ.setparam(group.p, group.q)
+
 (pk, sk) = keygen()
 signer = Signer(sk)
 verifier = Verifier(pk)
@@ -51,25 +54,3 @@ message = "Mama tata"
 sigma = signer.sign(message)
 result = verifier.verify(sigma, message)
 print(result)
-
-'''
-pseudokod
-sk = a <-$Zq, pk = A <- g^a
-
-
-Signer (m,a)
-r<-$ {0,1}^ml
-h<- H1(m,r)
-H = h^a
-x <-$ Zq*
-R = g^x , X = h^x
-c <- H2(m,r,x,A,H,R)
-s = x + ac
-sigma(R,X,H,s,r)
-
-Verifier (sigma, m,A)
-h <- H1(m,r)
-c <- H2(m,R,X,A,H,r)
-verification : g^s == RH^c && H^s  == XH^c
-
-'''
